0243a770abe6cf89b3579ab6811005405651173f
shin-f0318 <sf.0318ss@gmail.com>
2022-12-18 15:30:00 +0900

    ajax commit

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SpaApiController;
+use App\Http\Controllers\Api\SpaApiController;
 
 /*
 |--------------------------------------------------------------------------
